c76d6e57a19c506a633528bd2fba758382c5a144
SOLUDOCTUS\albeiro.marin <albeiro.marin@doctus.com.co>
2021-09-14 08:58:11 -0500

    Crea la tabla de usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app.routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+//ngprime components
+import {TableModule } from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+
 import { AppComponent } from './app.component';
 import { IndividualUserComponent } from './users/individual-user/individual-user.component';
 import { AllUsersComponent } from './users/all-users/all-users.component';
@@ -18,7 +22,9 @@ import { AllUsersComponent } from './users/all-users/all-users.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    TableModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
